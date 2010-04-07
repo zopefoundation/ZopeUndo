7b07416dca038b192e556c7fab8ee1f6df020e5d
Tres Seaver <tseaver@palladion.com>
2010-04-07 16:39:36 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2010 Zope Corporation and Contributors.
+# Copyright (c) 2010 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -19,7 +19,7 @@ setup(name='ZopeUndo',
       url='http://pypi.python.org/pypi/ZopeUndo',
       license='ZPL 2.1',
       description="ZODB undo support for Zope2.",
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       long_description=open('README.txt').read() + '\n' +
                        open('CHANGES.txt').read(),
