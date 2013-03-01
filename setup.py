9234b4dc01bafa74715994f8fd3037262d227dc9
Hanno Schlichting <hanno@hannosch.eu>
2013-03-01 23:54:46 +0100

    give readme/changes a .rst ending, bump version

--- setup.py
+++ setup.py
@@ -12,20 +12,27 @@
 #
 ##############################################################################
 
+import os
 from setuptools import setup, find_packages
 
-setup(name='ZopeUndo',
-      version = '2.12.1dev',
-      url='http://pypi.python.org/pypi/ZopeUndo',
-      license='ZPL 2.1',
-      description="ZODB undo support for Zope2.",
-      author='Zope Foundation and Contributors',
-      author_email='zope-dev@zope.org',
-      long_description=open('README.txt').read() + '\n' +
-                       open('CHANGES.txt').read(),
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.rst')) as f:
+    CHANGES = f.read()
 
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      include_package_data=True,
-      zip_safe=False,
-      )
+
+setup(
+    name='ZopeUndo',
+    version='4.0dev',
+    url='http://pypi.python.org/pypi/ZopeUndo',
+    license='ZPL 2.1',
+    description="ZODB undo support for Zope2.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=README + '\n\n' + CHANGES,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+)
