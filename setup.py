ba2863c29de8c115222b760e23dffb277804ccc1
Hanno Schlichting <hanno@hannosch.eu>
2017-04-26 18:42:29 +0200

    vb [ci skip]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = '4.2'
+__version__ = '4.3.dev0'
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst')) as f:
